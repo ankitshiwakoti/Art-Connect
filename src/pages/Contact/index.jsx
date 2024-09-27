b59b0e498744265d8aa0679bcf75aae68f2e2b95
rupinder kaur <rupinderone3@gmail.com>
2024-09-27 14:02:26 -0600

    changed on placeholder of contact page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -14,11 +14,11 @@ function Contact() {
                     </div>
                     <div>
                         {/* <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label> */}
-                        <input type="email" id="email" name="email" placeholder='Enter your Email' className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
+                        <input type="email" id="email" name="email" placeholder='Enter your Name' className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
                     </div>
                     <div>
                         {/* <label htmlFor="message" className="block text-sm font-medium text-gray-700">Message</label> */}
-                        <textarea id="message" name="message" placeholder='Enter your Email' rows={4} className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"></textarea>
+                        <textarea id="message" name="message" placeholder='Subject' rows={4} className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"></textarea>
                     </div>
                     <div>
                         <button type="submit" className="w-full bg-black text-white px-6 py-2 rounded hover:bg-gray-800">Send Message</button>
