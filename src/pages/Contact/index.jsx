a70d3fd05c9b4705f076300163f0bc25bb368c22
Ankit Shiwakoti <shiwakotiankit@gmail.com>
2024-09-27 15:19:18 -0500

    validation on contact page using html

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -10,11 +10,11 @@ function Contact() {
                 <form className="space-y-4">
                     <div>
                         {/* <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label> */}
-                        <input type="text" id="name" name="name" placeholder='Enter your Email' className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
+                        <input type="text" required id="name" name="name" placeholder='Enter your Name' className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
                     </div>
                     <div>
                         {/* <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label> */}
-                        <input type="email" id="email" name="email" placeholder='Enter your Name' className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
+                        <input type="email" required id="email" name="email" placeholder='Enter your Email' className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
                     </div>
                     <div>
                         {/* <label htmlFor="message" className="block text-sm font-medium text-gray-700">Message</label> */}
