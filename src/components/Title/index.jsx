ee8e7edfae3cc62d8be79b59a76b05867adc140f
wangyue <wy950408@gmail.com>
2024-10-01 20:37:15 -0500

    title

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,5 +1,7 @@
 const Title = ({ children }) => {
-    return <h1 className="text-4xl font-bold mb-8 h-48 content-center bg-black text-white w-full">{children}</h1>
+    return <h1 className="
+    text-4xl md:text-5xl lg:text-6xl font-bold mb-8 h-48 content-center bg-black text-white w-full
+    ">{children}</h1>
 }
 
 export default Title;
